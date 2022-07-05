46698edcf20dd8d468a32d7a3c116ef0373f82b8
Aravell1 <26070858+Aravell1@users.noreply.github.com>
2022-07-05 19:27:37 -0400

    1st Push

---
+++ Source/VoidSpoken/VoidSpokenGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoidSpokenGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOIDSPOKEN_API AVoidSpokenGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
