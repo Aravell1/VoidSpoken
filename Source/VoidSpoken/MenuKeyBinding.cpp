8fe63faf93c446e01464db9b26e4f52730fc9bed
YunYun <Janminecraft1234@gmail.com>
2022-07-14 17:18:27 -0400

    Settings menu and Credits added
    +Credits media added (Template)
    +Settings menu redone
    +Class of MenuControls futher more implemented to handle better usability

--- Source/VoidSpoken/MenuKeyBinding.cpp
+++ Source/VoidSpoken/MenuKeyBinding.cpp
@@ -2,4 +2,23 @@
 
 
 #include "MenuKeyBinding.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+#include "Styling/SlateBrush.h"
 
+void UMenuKeyBinding::NativePreConstruct(bool IsDesignTime) {
+	Super::PreConstruct(IsDesignTime);
+	Image->SetBrush(ImageParameters);
+
+	ActionMap->SetColorAndOpacity(ImageParameters.TintColor);
+	ActionMap->SetText(FText::FromString(": " + ActionString));
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("PreConstucted!"));
+}
+
+void UMenuKeyBinding::NativeConstruct() {
+	Super::NativeConstruct();
+	Image -> SetBrush(ImageParameters);
+
+	ActionMap -> SetColorAndOpacity(ImageParameters.TintColor);
+	ActionMap -> SetText(FText::FromString(": " + ActionString));
+}
--- Source/VoidSpoken/MenuKeyBinding.h
+++ Source/VoidSpoken/MenuKeyBinding.h
@@ -1,34 +1,44 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+#include "SlateBasics.h"
+#include "SlateExtras.h"
 #include "MenuKeyBinding.generated.h"
 
 /**
  * 
  */
-UCLASS()
-class VOIDSPOKEN_API UMenuKeyBinding : public UUserWidget
+
+UCLASS(Blueprintable)
+class UMenuKeyBinding : public UUserWidget
 {
 	GENERATED_BODY()
+
+private:
+	//Image Widget Object Reference
+	UPROPERTY(VisibleInstanceOnly)
+		UImage* Image;
+
+	//The TextBox Object Reference
+	UPROPERTY(VisibleInstanceOnly)
+		UTextBlock* ActionMap;
 
 public:
 	//Color of what the given Image and Text should be
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		FColor Tint;
+		FSlateBrush ImageParameters;
 
 	//Size of the Given Image
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		FVector2D ImageSize = {40, 40};
+		FVector2D ImageSize;
 
-	//Selected 2D Texture for the Image (On Canvas) to have
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		UTexture2D* ControllerSprite = NULL;
-	
 	//The String alongside the Image describing the functionallity of this action
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		FString ActionKeyString = "None";
+		FString ActionString;
 
+	//Functions
+	virtual void NativePreConstruct(bool IsDesignTime);
+	virtual void NativeConstruct();
 };
