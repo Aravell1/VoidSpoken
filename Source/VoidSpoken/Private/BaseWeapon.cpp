a421ebc2ee9e38f598fc84fcc01baee11d4fdaa3
YunYun <Janminecraft1234@gmail.com>
2022-08-31 01:03:39 -0400

    BaseWeapon Update
    User Interface:
    +Linked the UStatsMasterClass to the HUD element and seems to be working fine.
    ~Did not test, since there is no way to test between player and enemies, could also make one but also too lazy to.
    
    BaseWeapon:
    +Added Being/End Overlap Events inside of the BaseWeapon Class
    +Added Hit Event inside of the BaseWeapon Class
    +Added a Box Collider for the Developer to interact and modify the collision hit
    +More Comments and Detailed descriptions of Functions and Variables
    +Added New GetEquippedCharacter() function
    
    Plans:
    ~Moving Away from the FTimerHandle usage and going to get into making a UAnimNotify and UAnimNotifyState System for animators and developers to add and connect functionalities together with ease
    ~Involves adding 2 new classes (One for the UAnimNotify, and one for the UAnimNotifyState), since these classes notifies (when called) appear on the SkeletalMesh that it's applied on, making a SkeletalMesh strictly for the player and other Humanoid entities in the game will allow this system to be easily adapted over. These can also work (in theory) with other SkeletalMesh(s) if this works how I think.
    
    -UAnimNotify will create simple Instantaneous Events, used for hitting Enemies with attacks and etc
    -UAnimNotifyStates will  create windows of conditions using the UAnimNotifyState::OnBegin() and UAnimNotifyState::OnEnd() delegates.
    ---E.x: Create a system that can dictate when the player can attack, or use their UniqueAttack (if there is one on the weapon)
    
    ***********************************
    The Documentation will Be issued soon on the GDD of this game, I'm just trying to catch up on some assignments so it might be a bit late on adding it. Although if I do forget to add it, please give me shit :smile:

--- Source/VoidSpoken/Private/BaseWeapon.cpp
+++ Source/VoidSpoken/Private/BaseWeapon.cpp
@@ -9,8 +9,14 @@ ABaseWeapon::ABaseWeapon()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	WeaponMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Weapon Mesh Component"));
+	BoxColliderComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collider Component"));
 	WeaponMeshComponent->SetHiddenInGame(false, true);
+
 	RootComponent = WeaponMeshComponent;
+
+	BoxColliderComponent->SetCollisionProfileName(TEXT("Box Trigger"));
+	BoxColliderComponent->SetGenerateOverlapEvents(true);
+	BoxColliderComponent->SetNotifyRigidBodyCollision(true);
 }
 
 // Called when the game starts or when spawned
@@ -18,6 +24,14 @@ void ABaseWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ABaseWeapon::PostInitializeComponents() {
+	Super::PostInitializeComponents();
+
+	BoxColliderComponent->OnComponentBeginOverlap.AddDynamic(this, &ABaseWeapon::OnComponentBeginOverlap);
+	BoxColliderComponent->OnComponentHit.AddDynamic(this, &ABaseWeapon::OnComponentHit);
+	BoxColliderComponent->OnComponentEndOverlap.AddDynamic(this, &ABaseWeapon::OnComponentEndOverlap);
 }
 
 // Called every frame
@@ -36,51 +50,97 @@ int ABaseWeapon::GetCurrentComboIndex() {
 }
 
 int ABaseWeapon::GetComboLength() {
-	return this->ComboMontage.Max();
+	return this->ComboAnimationMontage.Max();
+}
+
+ACharacter* ABaseWeapon::GetEquippedCharacter() {
+	return this->EquippedCharacter;
 }
 
 void ABaseWeapon::Equip(ACharacter* EquippingActor) {
-	EquippedActor = EquippingActor;
+	EquippedCharacter = EquippingActor;
+	SetOwner(EquippingActor);
+	DefaultAnimInstance = EquippingActor->GetMesh()->GetAnimInstance();
+	EquippedCharacterMovementComponent = EquippingActor->GetCharacterMovement();
 }
 
 void ABaseWeapon::Attack() {
 	if (CurrentComboIndex >= GetComboLength()) Reset();
 
+	//Disabling Actors movement while attacking
+	EquippedCharacterMovementComponent->SetMovementMode(MOVE_None);
+
+	//Managing the Timers and Delays
 	if (GetWorldTimerManager().IsTimerActive(ResetDelay)) GetWorldTimerManager().ClearTimer(ResetDelay);
-	if (GetWorldTimerManager().GetTimerRemaining(AttackDelay) <= 0.0f) {
-		EquippedActor->GetMesh()->PlayAnimation(ComboMontage[CurrentComboIndex], false);
-		GetWorldTimerManager().SetTimer(AttackDelay, this, &ABaseWeapon::NextAttack, ComboMontage[CurrentComboIndex]->GetPlayLength(), false);
+	if (GetWorldTimerManager().GetTimerRemaining(AttackDelay) <= 0.0f && ComboAnimationMontage.IsValidIndex(CurrentComboIndex)) {
+		EquippedCharacter->GetMesh()->PlayAnimation(ComboAnimationMontage[CurrentComboIndex], false);
+		GetWorldTimerManager().SetTimer(AttackDelay, this, &ABaseWeapon::NextAttack, ComboAnimationMontage[CurrentComboIndex]->GetPlayLength(), false);
 	}
+}
+
+void ABaseWeapon::DealDamage() {
+
 }
 
 void ABaseWeapon::NextAttack() {
 	//Checking if the Blending Array is valid
 	if (ComboBlendingMontage.IsValidIndex(CurrentComboIndex)) {
-		EquippedActor->GetMesh()->PlayAnimation(ComboBlendingMontage[CurrentComboIndex], false);
+		EquippedCharacter->GetMesh()->PlayAnimation(ComboBlendingMontage[CurrentComboIndex], false);
+		
+		//Setting the ResetDelay to become active during the blending animation's length,
 		GetWorldTimerManager().SetTimer(ResetDelay, this, &ABaseWeapon::Reset, ComboBlendingMontage[CurrentComboIndex]->GetPlayLength(), false);
 	}
 	else {
-		GEngine->AddOnScreenDebugMessage(0, 2.0f, FColor::Red, "Blending Combo Montage Array[" + FString::FromInt(CurrentComboIndex) + "] Not Intialized");
+		GEngine->AddOnScreenDebugMessage(0, 2.0f, FColor::Red, "Blending Combo Sequence Array[" + FString::FromInt(CurrentComboIndex) + "] Not Intialized");
 		
-		//Setting the ResetDelay to become active during the blending animation's length,
-		GetWorldTimerManager().SetTimer(ResetDelay, this, &ABaseWeapon::Reset, 1.25f, false);
+		GetWorldTimerManager().SetTimer(ResetDelay, this, &ABaseWeapon::Reset, 2.25f, false);
 	}
+
+	//Re-Enabling Actors movement after attacking
+	EquippedCharacterMovementComponent->SetMovementMode(MOVE_Walking);
 
 	CurrentComboIndex++;
 }
 
 void ABaseWeapon::UniqueAttack() {
+	//Disabling Actors movement while attacking
+	EquippedCharacterMovementComponent->SetMovementMode(MOVE_None);
+
 	//Vailidating the UniqueAttackSequence
 	if (GetWorldTimerManager().GetTimerRemaining(AttackDelay) > 0.0f && UniqueAttackMontage) {
-		EquippedActor->GetMesh()->PlayAnimation(UniqueAttackMontage, false);
+		EquippedCharacter->GetMesh()->PlayAnimation(UniqueAttackMontage, false);
 		GetWorldTimerManager().SetTimer(AttackDelay, this, &ABaseWeapon::NextAttack, UniqueAttackMontage->GetPlayLength(), false);
+		GetWorldTimerManager().SetTimer(UniqueAttackDelay, this, &ABaseWeapon::Reset, UniqueAttackMontage->GetPlayLength() + UniqueAttackCooldown, false);
 	}
-
-	Reset();
 }
 
 void ABaseWeapon::Reset() {
 	GEngine->AddOnScreenDebugMessage(0, 2.0f, FColor::Green, "Resetted Combo");
-	GetWorldTimerManager().ClearTimer(AttackDelay);
+	GetWorldTimerManager().ClearTimer(ResetDelay);
+	EquippedCharacter->GetMesh()->AnimScriptInstance = DefaultAnimInstance;
 	CurrentComboIndex = 0;
+
+	//Re-Enabling Actors movement just in case the attacks do not reset the characters movement
+	EquippedCharacterMovementComponent->SetMovementMode(MOVE_Walking);
+	EquippedCharacter->GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+}
+
+void ABaseWeapon::OnComponentBeginOverlap(class UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this) && OtherComponent)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap Begin"));
+	}
+}
+
+void ABaseWeapon::OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex)
+{
+	if (OtherActor && (OtherActor != this) && OtherComponent)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap End"));
+	}
+}
+
+void ABaseWeapon::OnComponentHit(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit) {
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Component Hit"));
 }
--- Source/VoidSpoken/Public/BaseWeapon.h
+++ Source/VoidSpoken/Public/BaseWeapon.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "GameFramework/Character.h"
 #include "Animation/AnimMontage.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/BoxComponent.h"
 #include "BaseWeapon.generated.h"
 
 UCLASS(Abstract)
@@ -17,55 +19,107 @@ public:
 	// Sets default values for this actor's properties
 	ABaseWeapon();
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void PostInitializeComponents() override;
+
+	//Getter Statements
+
+	/// Function: GetBaseDamage()
+	///		-Returns the float BaseDamage
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+		float GetBaseDamage();
+
+	/// Function: GetCurrentComboIndex()
+	///		-Returns the int CurrentComboIndex
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+		int GetCurrentComboIndex();
+
+	/// Function: GetComboLength()
+	///		-Returns an int of the Max() of Elements inside the ComboAnimationMontage
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+		int GetComboLength();
+
+	/// Function: GetEquippedCharacter() 
+	///		-Returns a ACharacter* of the variable EquippedCharacter
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+		ACharacter* GetEquippedCharacter();
+
+	//Weapon Functions
+
+	/// Function: Equip(ACharacter* EquippingCharacter) 
+	///		-This functions sets the variable EquippedCharacter
+	///		-Will Also set this Weapon's owner as EquippedCharacter
+	///		-Initializes the UAnimInstance from the Default 
+	///			*(At the time of this Equip(), what was the UAnimInstance the Character had)
+	///		-Initializes the UCharacterMovementComponent from the given EquippedCharacter 
+	UFUNCTION(BlueprintCallable)
+		void Equip(ACharacter* EquippingCharacter);
+
+	/// Function: Attack()
+	///		-Momentarly disables player movement
+	///		-Set Timers (AttackDelay) to be active and delays for the respective UAnimMontage's PlayLength()
+	///		-Will not play any animations if the ComboAnimationMontage are not set 
+	UFUNCTION(BlueprintCallable)
+		void Attack();
+
+	/// Function: DealDamage()
+	///		-Checks if the Attached Box Collider is overlapping any Actors (Preferably if their from a BaseEntity class)
+	///		-Length of this function is determined in each of the ComboAnimationMontage
+	///		-Uses the Notify and NotifyState systems in each Animation
+	UFUNCTION(BlueprintCallable)
+		void DealDamage();
+
+	/// Function: NextAttack()
+	///		-Re-Enables the player movement
+	///		-Sets another Timer (ResetDelay), which calls Reset()
+	///		-Itterates the CurrentComboCount
+	UFUNCTION(BlueprintCallable)
+		void NextAttack();
+
+	/// Function: UniqueAttack()
+	///		-Disables the player movement
+	///		-Plays the UniqueAnimationMontage
+	///		-After UniqueAnimationMontage is done, then Plays the UniqueBlendingMontage
+	///		-Sets Timer (NextAttack)
+	///		-Sets Timer (UniqueAttackDelay)
+	UFUNCTION(BlueprintCallable)
+		void UniqueAttack();
+
+	/// Function: Reset() 
+	///		-Resets the player back into their Default Animation Instance
+	///		-Sets CurrentComboIndex to 0
+	///		-Enables Player Movement
+	///		-Clears Timers (AttackDelay, ResetDelay)
+	///	*NOTES*
+	///		Only Use when the player either has been inturupted (via rolling, falling, stunned, etc)
+	UFUNCTION(BlueprintCallable)
+		void Reset();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 	//Weapon Parameters
-	
+
 	/// Base Damage of this Weapon
 	///		-Any scaling involving the damage of the Attack, this value will influence
 	///		-Can be set to any float value
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = ( NoGetter ))
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (NoGetter))
 		float BaseDamage = 0;
 
 	/// The Actor that has equipped this weapon
 	///		-For purposes of unwanted self damage infliction
 	///		-This will also be used to equip to this Actor
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		ACharacter* EquippedActor;
-
-	//Getter Statements
-
-	UFUNCTION(BlueprintPure, BlueprintCallable)
-		float GetBaseDamage();
-	UFUNCTION(BlueprintPure, BlueprintCallable)
-		int GetCurrentComboIndex();
-	UFUNCTION(BlueprintPure, BlueprintCallable)
-		int GetComboLength();
-
-	//Weapon Functions
-
-	UFUNCTION(BlueprintCallable)
-		void Equip(ACharacter* EquippingActor);
-	UFUNCTION(BlueprintCallable)
-		void Attack();
-	UFUNCTION(BlueprintCallable)
-		void NextAttack();
-	UFUNCTION(BlueprintCallable)
-		void UniqueAttack();
-	UFUNCTION(BlueprintCallable)
-		void Reset();
-
-	//Timer Handles
-	FTimerHandle AttackDelay;
-	FTimerHandle ResetDelay;
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+		ACharacter* EquippedCharacter;
 
 	/// This Array contains all the Animation Montages for the Attack of this weapon.
 	///		-This cannot be unintialized
-	///		-This will loop constantly when this runs through the whole Sequence
+	///		-This will loop constantly when this runs through the whole Montage
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-		TArray<UAnimMontage*> ComboMontage = {};
+		TArray<UAnimMontage*> ComboAnimationMontage = {};
 
 	/// Contains all the Animations for the Attack(s) after being played.
 	///		-These are in their respective order with the ComboMontage Elements
@@ -96,19 +150,55 @@ protected:
 
 	/// Where will this window of opporunity be opened at
 	///		-This can happen anywhere from 0 to ComboMontage - 1.
-	///		-Will not play if UniqueAnimationSequence is null, or unintialized
+	///		-Will not play if UniqueAnimationMontage is null, or unintialized
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
 		int UniqueAttackIndex = 0;
 
+	/// The cooldown (in seconds) after the attack has been initiated
+	///		-This is additive of the UniqueAttackMontage's play length (in seconds)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		float UniqueAttackCooldown = 0;
+
+	/// Components of this class
+
+	/// Box Collider
+	///		-This collider will always Ignore the Owning Actor/Character
+	///		-The Bounds of this box must be set in-engine
+	UPROPERTY(EditDefaultsOnly)
+		class UBoxComponent* BoxColliderComponent = nullptr;
+	
 	/// Mesh of this weapon
 	///		-Can be uninitialized, although not recommended
 	///		-Collision will be based off of this mesh (Simple Collision)
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		class UStaticMeshComponent* WeaponMeshComponent;
-		
 
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	/// Actors UAnimMontage or UAnimBlueprint after they've been reset (Bringing them to their default state)
+	UPROPERTY(BlueprintReadOnly)
+		class UAnimInstance* DefaultAnimInstance;
+	
+	//Equipped Actors movement component to be set disabled and re-enabled when attacking
+	UPROPERTY(BlueprintReadOnly)
+		class UCharacterMovementComponent* EquippedCharacterMovementComponent;
 
+	//Timer Handles
+
+	//Timer Handler for NextAttack()
+	FTimerHandle AttackDelay;
+
+	//Timer Handler for UniqueAttack()
+	FTimerHandle UniqueAttackDelay;
+
+	//Timer Handler for Reset()
+	FTimerHandle ResetDelay;
+
+	//Collision Delegates
+	UFUNCTION()
+		void OnComponentBeginOverlap(class UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+		void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex);
+	
+	UFUNCTION()
+		void OnComponentHit(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
