c05da606e135698799e80a7635d84f5be0bb1084
CODman-2019 <55792105+CODman-2019@users.noreply.github.com>
2022-11-26 02:03:44 -0500

    Methods and variables created Part 1

--- Source/VoidSpoken/Private/MeleeEnemy.cpp
+++ Source/VoidSpoken/Private/MeleeEnemy.cpp
@@ -1,15 +1,54 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MeleeEnemy.h"
+#include "Kismet/GameplayStatics.h"
 
 //AMeleeEnemy::AMeleeEnemy() 
 //{
 //	/*PatrolIndex = 0;
-//	AttackRange = 50.0f;*/
+//	*/
 //}
 
 AMeleeEnemy::AMeleeEnemy()
 {
+	//find the player if null
+	//PlayerTarget = UGameplayStatics::GetActorOfClass(GetWorld(), TSubClassOf<APlayerController>);
+
+	AttackRange = 50.0f;
+}
+
+void AMeleeEnemy::ChangeStates(MeleeBehaviourState NewState)
+{
+	CurrentBehaviour = NewState;
+}
+
+void AMeleeEnemy::ChangePatrolPoints()
+{
+
+}
+
+void AMeleeEnemy::ChasePlayer()
+{
+
+}
+
+void AMeleeEnemy::Search()
+{
+
+}
+
+void AMeleeEnemy::Die() 
+{
+
+}
+
+void AMeleeEnemy::DropItem()
+{
+
+}
+
+void AMeleeEnemy::BeginPlay()
+{
+	Super::BeginPlay();
 
 }
--- Source/VoidSpoken/Public/MeleeEnemy.h
+++ Source/VoidSpoken/Public/MeleeEnemy.h
@@ -4,20 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "BaseEnemy.h"
-//#include "Animation/AnimMontage.h"
+#include "Animation/AnimMontage.h"
 //#include "Kismet/GameplayStatics.h"
 #include "Containers/Array.h"
 #include "MeleeEnemy.generated.h"
 
-//UENUM()
-//enum MeleeBehaviourState
-//{
-//	Patrol	UMETA(DisplayName = "Patrol"),
-//	Chase	UMETA(DisplayName = "Chase"),
-//	Attack	UMETA(DisplayName = "Attack"),
-//	Search	UMETA(DisplayName = "Search"),
-//	Dead	UMETA(DisplayName = "Dead")
-//};
+UENUM()
+enum MeleeBehaviourState
+{
+	Patrol	UMETA(DisplayName = "Patrol"),
+	Chase	UMETA(DisplayName = "Chase"),
+	Attack	UMETA(DisplayName = "Attack"),
+	Search	UMETA(DisplayName = "Search"),
+	Dead	UMETA(DisplayName = "Dead")
+};
 
 /**
  * 
@@ -30,26 +30,56 @@ class VOIDSPOKEN_API AMeleeEnemy : public ABaseEnemy
 public:
 	AMeleeEnemy();
 
+	void ChangeStates(MeleeBehaviourState NewState);
+	void ChangePatrolPoints();
+	void ChasePlayer();
+	void Search();
+	void Die();
+	void DropItem();
+
+	void BeginPlay();
+
 	UPROPERTY(EditAnywhere)
-	bool customPoints;
-	UPROPERTY(VisibleAnyWhere, EditAnywhere, Category = PatrolPoints)
-		TArray<AActor*> PatrolPoints;
+		bool findPoints = false;
+
+	UPROPERTY(EditAnywhere, Category = PatrolPoints)
+	TArray<AActor*> PatrolPoints;
+
+	UPROPERTY(EditAnywhere, Category = ItemDrops)
+	TArray<AActor*> ItemDrops;
+
+	UPROPERTY(EditAnywhere)
+	AActor* PlayerTarget = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	TArray<UAnimMontage*> AttackAnims;
+
+	UPROPERTY(EditAnywhere)
+	UAnimMontage* HeadExplosion = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	float AttackRange = 0.0f;
 
 private:
-	APawn* Player = nullptr;
+	MeleeBehaviourState CurrentBehaviour = Patrol;
+	int PatrolIndex;
 
 //
 //public:
 //	AMeleeEnemy();
+// 
 //	// list of patrol points
 //	TArray<APawn> PatrolPoints;
 // 
 //	// player target
 //	APawn* Player;
+// 
 //	//list of animations
 //	TArray<UAnimMontage> AttackAnims;
+// 
 //	//head exploding animation
 //	UAnimMontage HeadExplosion;
+// 
 //	//attack range
 //	float AttackRange;
 //
@@ -61,9 +91,12 @@ private:
 //		attack,
 //		search
 //	};*/
+// 
 //	// current behaviour
 //	//Behaviour current = Behaviour.patrol;
+// 
 //	//MeleeBehaviourState CurrentBehaviour = Patrol;
+// 
 //	// location
 //	int PatrolIndex;
 
