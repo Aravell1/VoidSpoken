efeadcdda197cebd44fabb6504d90ce48887ed97
Aravell1 <26070858+Aravell1@users.noreply.github.com>
2023-02-02 22:29:15 -0500

    Revert "Revert Inventory System Commit"
    This reverts commit 7c41d2e587828e9b84f4a824cecf04bfba02be28.

--- Source/VoidSpoken/Public/StaminaPickup.h
+++ Source/VoidSpoken/Public/StaminaPickup.h
@@ -8,6 +8,7 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/TextRenderComponent.h"
+#include "Components/BoxComponent.h"
 #include "StaminaPickup.generated.h"
 
 /**
@@ -22,13 +23,23 @@ public:
 	//Constructor
 	AStaminaPickup();
 
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Text")
+	UTextRenderComponent* Text;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Text | Trigger")
+	UBoxComponent* TextTriggerBox;
+
 	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	//virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 	void PickupStamina();
 	
+	UFUNCTION()
+	void TextTriggerOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Pickup Text")
-	UTextRenderComponent* PickupText;
+	UFUNCTION()
+	void TextTriggerOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
