de33623bcdd9ddb8cc42069f934a9cccafba665a
YunYun <Janminecraft1234@gmail.com>
2023-02-22 12:29:31 -0500

    Getting Animations branched in

---
+++ Source/VoidSpoken/Public/PlayerMovementInterface.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "PlayerMovementInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UPlayerMovementInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class VOIDSPOKEN_API IPlayerMovementInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
