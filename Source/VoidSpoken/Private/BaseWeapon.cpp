c906315fc90959fe83aa50598b9e56ad375a450a
YunYun <Janminecraft1234@gmail.com>
2023-03-16 23:46:08 -0400

    UI Skips and Videos

--- Source/VoidSpoken/Private/BaseWeapon.cpp
+++ Source/VoidSpoken/Private/BaseWeapon.cpp
@@ -176,7 +176,7 @@ void ABaseWeapon::Reset() {
 
 void ABaseWeapon::OnComponentBeginOverlap(class UPrimitiveComponent* OverlappedComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (bCheckForOverlappedActors && OtherActor && (OtherActor != this) && (OtherActor != EquippedCharacter) && OtherComponent) {
+	if (bAttackDelay && bCheckForOverlappedActors && OtherActor && (OtherActor != this) && (OtherActor != EquippedCharacter) && OtherComponent) {
 		if (!OverlappedActors.Contains(OtherActor)) {
 			DealDamage(OtherActor);
 			OverlappedActors.AddUnique(OtherActor);
